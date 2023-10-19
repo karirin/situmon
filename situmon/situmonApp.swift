b729e1667774b9e21e5a0012181f0bf4348d31a6
karirin <karirin419@gmail.com>
2023-10-20 00:52:33 +0900

    Initial Commit

---
+++ situmon/situmonApp.swift
@@ -0,0 +1,17 @@
+//
+//  situmonApp.swift
+//  situmon
+//
+//  Created by hashimo ryoya on 2023/10/20.
+//
+
+import SwiftUI
+
+@main
+struct situmonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
